0859ddd95ff5fd7e607139ef87066331e00c4748
Travis Golia <travis@gigtown.com>
2016-11-18 16:59:15 -0800

    v5, final version before weekend

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -12,10 +12,10 @@
     function HomeController() {
     	var vm = this;
 
-        var cssMap = {
-            3: 'panel-info',
-            2: 'panel-warning',
-            1: 'panel-danger'
+        var cssMap = { 
+            3: 'panel-info', 
+            2: 'panel-warning', 
+            1: 'panel-danger' 
         };
 
         vm.getClass = function(todo) {
@@ -23,22 +23,27 @@
         }
 
     	vm.todos = [];
-        vm.orderedList = '';
 
         var sortArray = ['name','priority','-priority'];
 
         vm.sortTodos = function(order) {
-            vm.orderedList = sortArray[order];
+            vm.orderCategory = sortArray[order];
         }
 
 
     	vm.addTodo = addTodo;
 
-    	//////////////// There's a short cut to create a line break like this ('line-break').... might need to add files to Sublime package like I did with John Papa Angular Snippets (video in Resilio)
+    	// TODO: Review John Papa formatting to put functions below line break
+        //////////////// There's a short cut to create a line break like this ('line-break').... might need to add files to Sublime package like I did with John Papa Angular Snippets (video in Resilio)
 
-    	function addTodo() {
-            var copy = angular.copy(vm.newTodo); // angular.copy creates clone (deep copying) of object
-    		vm.todos.push(copy); // unshift to put at opposite side
+    	function addTodo() { 
+            if (vm.newTodo.priority == undefined) {
+                alert("Please add a priority");  
+            } else {
+                var copy = angular.copy(vm.newTodo); // angular.copy creates clone (deep copying) of object
+                vm.todos.push(copy); // unshift to put at opposite side
+            }
     	}
+
     }
 })();
